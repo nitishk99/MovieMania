962f083207fe43560e707aeb9f6da28a7847d12d
nitishk99 <nitishkumarhere@gmail.com>
2024-01-08 19:53:21 +0530

    Fixed logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
         <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
             <ContentWrapper>
                 <div className="logo" onClick={() => navigate("/")}>
-                    //<img src={logo} alt="" />
+                    <img src={logo} alt="" />
                 </div>
                 <ul className="menuItems">
                     <li
